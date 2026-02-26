366333e20db1fe79bb4bdfe57e0627ee3232de5f
Camila <camilamb567@gmail.com>
2026-02-26 09:39:22 -0500

    Primer commit proyecto Flask

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template('inicio.html')
+
+@app.route("/suma", methods=["GET", "POST"])
+def sumar():
+    if request.method == "POST":
+        if not request.form.get("num1") or not request.form.get("num2"):
+            return f"Por favor, ingrese dos números válidos."
+        else:
+            num1 = float(request.form.get("num1"))
+            num2 = float(request.form.get("num2"))
+            resultado = num1 + num2
+            return render_template("suma.html", resultado=resultado)
+    return render_template("suma.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
